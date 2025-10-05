51da1b875397e9c7713b30eef21f6ca118775345
Kiju <dev@jakubkijek.com>
2025-10-05 23:08:36 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Rust DevShell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = {
+        packages = with pkgs; [
+          cargo
+          rustc
+        ];
+      };
+    };
+}
