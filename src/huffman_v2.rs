0979c7a2c8d42df480e064a75e059facb38d1de2
Kiju <dev@jakubkijek.com>
2025-10-13 19:47:54 +0200

    Finish the generic implementation

--- src/huffman_v2.rs
+++ src/huffman_v2.rs
@@ -4,6 +4,8 @@ use std::{
     hash::Hash,
 };
 
+use bitvec::vec::BitVec;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Tree<T> {
     Leaf {
@@ -38,20 +40,20 @@ impl<T: Clone + Eq> PartialOrd for Tree<T> {
     }
 }
 
-fn construct_freqs<T: Clone + Eq + Hash>(tokens: &Vec<T>) -> HashMap<T, u64> {
+pub fn construct_freqs<T: Clone + Eq + Hash>(tokens: &Vec<T>) -> HashMap<T, u64> {
     tokens.iter().fold(HashMap::new(), |mut freqs, t| {
         *freqs.entry(t.clone()).or_insert(0) += 1;
         freqs
     })
 }
 
-fn construct_tree<T: Clone + Eq + Ord>(freqs: HashMap<T, u64>) -> Tree<T> {
+pub fn construct_tree<T: Clone + Eq + Ord>(freqs: &HashMap<T, u64>) -> Tree<T> {
     let mut heap = BinaryHeap::new();
 
     for (token, freq) in freqs {
         heap.push(Reverse(Tree::Leaf {
-            freq: freq,
-            token: token,
+            freq: *freq,
+            token: token.clone(),
         }));
     }
 
@@ -68,4 +70,74 @@ fn construct_tree<T: Clone + Eq + Ord>(freqs: HashMap<T, u64>) -> Tree<T> {
     }
 
     heap.pop().unwrap().0
+}
+
+pub fn construct_encoder<T: Clone + Eq + Ord + Hash>(tree: &Tree<T>) -> HashMap<T, BitVec> {
+    let mut stack: Vec<(&Tree<T>, BitVec)> = vec![(tree, BitVec::new())];
+    let mut encoder = HashMap::new();
+
+    while stack.len() > 0 {
+        let (subtree, code) = stack.pop().unwrap();
+
+        match subtree {
+            Tree::Leaf { token, .. } => {
+                encoder.insert(token.clone(), code);
+            }
+            Tree::Node { left, right, .. } => {
+                let (mut code_left, mut code_right) = (code.clone(), code.clone());
+
+                code_left.push(false);
+                code_right.push(true);
+
+                stack.push((left, code_left));
+                stack.push((right, code_right));
+            }
+        }
+    }
+
+    encoder
+}
+
+pub fn encode_bit<T: Clone + Eq + Ord + Hash>(
+    encoder: &HashMap<T, BitVec>,
+    source: &Vec<T>,
+) -> BitVec {
+    let mut output = BitVec::new();
+
+    for token in source {
+        output.extend_from_bitslice(encoder.get(token).unwrap().as_bitslice());
+    }
+
+    output
+}
+
+pub fn decode_bit<T: Clone + Eq + Ord + Hash>(tree: &Tree<T>, source: &BitVec) -> Vec<T> {
+    let mut output = Vec::new();
+    let mut current_tree = tree.clone();
+    let mut i = 0;
+
+    while i < source.len() {
+        match current_tree {
+            Tree::Leaf { token, .. } => {
+                output.push(token.clone());
+                current_tree = tree.clone();
+            }
+            Tree::Node { left, right, .. } => {
+                if source[i] {
+                    current_tree = right.as_ref().clone();
+                } else {
+                    current_tree = left.as_ref().clone();
+                }
+
+                i += 1;
+            }
+        }
+    }
+
+    match current_tree {
+        Tree::Leaf { token, .. } => output.push(token.clone()),
+        Tree::Node { .. } => (),
+    }
+
+    output
 }
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,16 @@ mod huffman_v1;
 mod huffman_v2;
 
 fn main() {
-    let msg = String::from(
-        "I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those moments will be lost in time, like tears in rain. Time to die.",
-    );
-    let freqs = huffman_v1::construct_freqs(&msg);
-    let tree = huffman_v1::construct_huffman_tree(&freqs);
-    let encoder = huffman_v1::construct_encoder(&tree);
+    let source: Vec<char> = "aaabbc".chars().collect();
+    let freqs = huffman_v2::construct_freqs(&source);
+    let tree = huffman_v2::construct_tree(&freqs);
+    let encoder = huffman_v2::construct_encoder(&tree);
 
-    let msg_encoded = huffman_v1::encode(&encoder, &msg);
-    let msg_decoded = huffman_v1::decode(&tree, &msg_encoded);
+    let source_encoded = huffman_v2::encode_bit(&encoder, &source);
+    let source_decoded = huffman_v2::decode_bit(&tree, &source_encoded);
 
-    println!("msg_encoded: {:?}", msg_encoded);
-    println!("msg_decoded: {:?}", msg_decoded);
-    println!("---------------");
     println!(
-        "(in bits) msg len: {:?}, encoded len: {:?}",
-        msg.len() * 8,
-        msg_encoded.len()
+        "Source: {:?}\nTree: {:?}\nEncodedBit: {:?}\nDecodedBit: {:?}",
+        source, encoder, source_encoded, source_decoded
     );
 }
