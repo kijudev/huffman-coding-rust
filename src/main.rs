f068c7198218ce95102cd7ddc2d33bb0f143a6ff
Kiju <dev@jakubkijek.com>
2025-10-15 14:32:28 +0200

    Remove huffman_v1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-//mod huffman_v1;
-mod huffman_v2;
+mod huffman;
 
-fn main() {
-    let message = String::from("aaabbc");
-    let compressed = huffman_v2::encode(&message, |m| m.chars().collect());
-    let decompressed = huffman_v2::decode(&compressed, |cs: Vec<char>| cs.into_iter().collect());
-
-    println!(
-        "Message: {:?}\nCompressed: {:?}\n Decompressed: {:?}",
-        message, compressed, decompressed
-    );
-}
+fn main() {}
