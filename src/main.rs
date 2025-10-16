3e45015276faee6f2e9f5acdf05b8268c68b5802
Kiju <dev@jakubkijek.com>
2025-10-16 13:51:47 +0200

    Begin work on the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-mod huffman;
+use std::path::PathBuf;
 
-fn main() {}
+use clap::{Arg, command, value_parser};
+
+fn main() {
+    let matches = command!()
+        .arg(
+            Arg::new("file")
+                .required(true)
+                .value_parser(value_parser!(PathBuf)),
+        )
+        .get_matches();
+
+    if let Some(path) = matches.get_one::<PathBuf>("file") {
+        println!("{:?}", path);
+    }
+}
